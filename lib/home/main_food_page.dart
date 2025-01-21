28436ea1dca7b57368ea316ef8d5710bf210d425
zezo2030 <zezosaad194@gmail.com>
2025-01-21 13:29:28 +0200

    update

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
